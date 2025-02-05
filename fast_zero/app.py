4da3e4ca7ec1b1e2a1d2b93d404b462318d0e7df
Djalma Júnior <128833139+juniors719@users.noreply.github.com>
2025-02-05 16:38:48 +0000

    feat: :alembic: Resolução do exercício da aula 2
    Código do exercício da aula 2, que consiste em um endpoint de olá mundo que retorna um HTML, e a criação de um teste simples

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -6,3 +7,17 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'message': 'Hello World!'}
+
+
+@app.get('/ola_mundo', response_class=HTMLResponse)
+def ola_mundo_exercise():
+    return """
+        <html>
+        <head>
+            <title>Nosso olá mundo!</title>
+        </head>
+        <body>
+            <h1> Olá Mundo </h1>
+        </body>
+        </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,10 @@ def test_read_root_deve_retornar_ok_e_hello_world():
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello World!'}
+
+
+def test_exercicio_ola_mundo():
+    client = TestClient(app)
+    response = client.get('/ola_mundo')
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
