d1b62aeac100be33e521ed86c3f58f86060d166b
Djalma Júnior <junior.silva@alu.ufc.br>
2025-02-17 20:48:49 -0300

    feat: :sparkles: Adiciona metadados à instância do FastAPI, incluindo título, versão, descrição, informações de contato e licença

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -6,7 +6,27 @@ from fast_zero.schemas import (
     MessageSchema,
 )
 
-app = FastAPI()
+app = FastAPI(
+    title='API de Gerenciamento de Tarefas',
+    version="1.0.0",
+    description="""
+    Um projeto desenvolvido com FastAPI e gerenciado pelo Poetry,
+    que fornece uma API para o gerenciamento de tarefas e usuários.
+    A aplicação permite a criação, leitura, atualização e exclusão
+    (CRUD) de tarefas, além de funcionalidades completas para
+    a administração de usuários.
+    """,
+    contact={
+        "name": "Djalma Júnior",
+        "email": "junior.silva@alu.ufc.br",
+        "url": "https://github.com/seu-usuario/seu-repo",
+    },
+    license_info={
+        "name": "GNU General Public License v3.0",
+        "url": "https://www.gnu.org/licenses/gpl-3.0.html",
+    },
+)
+
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(todos.router)
